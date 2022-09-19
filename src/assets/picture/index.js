f6c15862d934fc83b7d05d7b65877a447ad8a003
datletrong <datletrong215@gmail.com>
2022-09-19 17:02:52 +0700

    [upd]: index

---
+++ src/assets/picture/index.js
@@ -0,0 +1,6 @@
+export { default as banhngot } from '@/assets/picture/banhngot-icon.png';
+export { default as dauhu } from '@/assets/picture/dauhu-icon.png';
+export { default as mi } from '@/assets/picture/mi-icon.png';
+export { default as pizza } from '@/assets/picture/pizza-icon.png';
+export { default as tom } from '@/assets/picture/tom-icon.png';
+export { default as top } from '@/assets/picture/top-dish.png';
